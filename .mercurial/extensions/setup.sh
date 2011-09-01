39020ec9b61f85a096647c587741c8ce65459aca
Takumi IINO <trot.thunder@gmail.com>
2011-09-01 11:37:51 +0900

    mercurial: update layout

---
+++ .mercurial/extensions/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+! [ -d 'hgshelve'     ] && hg clone https://bitbucket.org/tksoh/hgshelve
+! [ -d 'hgtasks'      ] && hg clone https://bitbucket.org/alu/hgtasks
+! [ -d 'histedit'     ] && hg clone https://bitbucket.org/spookylukey/histedit
+! [ -d 'hgsubversion' ] && hg clone https://bitbucket.org/durin42/hgsubversion/
+! [ -d 'hg-review'    ] && hg clone https://bitbucket.org/sjl/hg-review
+! [ -d 'hg-git'       ] && hg clone https://bitbucket.org/durin42/hg-git
