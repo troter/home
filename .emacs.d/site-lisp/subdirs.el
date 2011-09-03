dd153c835553c0db7d998178a606da06938e4484
Takumi IINO <trot.thunder@gmail.com>
2011-09-03 15:07:00 +0900

    emacs: rename from auto-complete to auto-complete-1.3.1

--- .emacs.d/site-lisp/subdirs.el
+++ .emacs.d/site-lisp/subdirs.el
@@ -6,7 +6,7 @@
   '("php-mode-1.5.0" "yasnippet-0.6.1c" "color-theme-6.6.0"
     "elscreen-1.4.6" "elscreen-dired-0.1.0" "apel-10.8"
     "ri-emacs"
-    "auto-complete"
+    "auto-complete-1.3.1"
     "rinari"))
 
 (setq load-path
