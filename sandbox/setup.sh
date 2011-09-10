720c76da8fad5d2243168b0396d6bf3db387cbe8
Takumi IINO <trot.thunder@gmail.com>
2011-09-10 09:13:46 +0900

    sandbox: update setup.sh

--- sandbox/setup.sh
+++ sandbox/setup.sh
@@ -1,9 +1,9 @@
+#!/bin/sh
 # -*- mode:sh; coding:utf-8-unix -*-
 
-git clone git@github.com:troter/troter.github.com.git
-git clone git@github.com:troter/s2jdbc-fluent-options.git
-git clone git@github.com:troter/s2jdbc-master-slave.git
-git clone git@github.com:troter/s2jdbc-custom-enum-type.git
-git clone git@github.com:troter/tags-capture.git
-git clone git@github.com:troter/log4j-rotate.git
-
+! [ -d 'troter.github.com'       ] && git clone git@github.com:troter/troter.github.com.git
+! [ -d 's2jdbc-fluent-options'   ] && git clone git@github.com:troter/s2jdbc-fluent-options.git
+! [ -d 's2jdbc-master-slave'     ] && git clone git@github.com:troter/s2jdbc-master-slave.git
+! [ -d 's2jdbc-custom-enum-type' ] && git clone git@github.com:troter/s2jdbc-custom-enum-type.git
+! [ -d 'tags-capture'            ] && git clone git@github.com:troter/tags-capture.git
+! [ -d 'log4j-rotate'            ] && git clone git@github.com:troter/log4j-rotate.git
