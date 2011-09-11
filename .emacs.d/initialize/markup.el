6c8fe0fb887330953a79bda88a8e206763b4ffd9
Takumi IINO <trot.thunder@gmail.com>
2011-09-12 01:03:28 +0900

    emacs: nxml-mode

--- .emacs.d/initialize/markup.el
+++ .emacs.d/initialize/markup.el
@@ -9,3 +9,15 @@
 (when (autoload-if-found 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
   (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
   (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode)))
+
+(when (autoload-if-found 'nxml-mode "nxml-mode.el" "a new XML mode" t)
+  (add-to-list 'auto-mode-alist '("\\.xml$" . nxml-mode))
+  (add-to-list 'auto-mode-alist '("\\.xsd$" . nxml-mode))
+  (add-to-list 'auto-mode-alist '("\\.xslt$" . nxml-mode))
+  (add-to-list 'auto-mode-alist '("\\.svg$" . nxml-mode))
+  (add-to-list 'auto-mode-alist '("\\.rss$" . nxml-mode))
+
+  (unify-8859-on-decoding-mode)
+
+  (fset 'xml-mode 'nxml-mode)
+  (fset 'html-mode 'nxml-mode))
