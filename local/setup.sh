2f7ef7a4d1239e6ec63f19046e43abb6239fbe64
Takumi IINO <trot.thunder@gmail.com>
2011-09-23 01:07:23 +0900

    local: update setup.sh

--- local/setup.sh
+++ local/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 ! [ -f ~/local/bin/ack                    ] && (curl http://betterthangrep.com/ack-standalone > ~/local/bin/ack && chmod 0755 ~/local/bin/ack)
+! [ -f ~/local/bin/ez_setup.py            ] && (curl http://peak.telecommunity.com/dist/ez_setup.py > ~/local/bin/ez_setup.py && chmod 0755 ~/local/bin/ez_setup.py)
 ! [ -f ~/local/src/ruby-1.9.2-p290.tar.gz ] && curl http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p290.tar.gz -o ~/local/src/ruby-1.9.2-p290.tar.gz
 ! [ -f ~/local/src/Python-3.2.2.tgz       ] && curl http://www.python.org/ftp/python/3.2.2/Python-3.2.2.tgz -o ~/local/src/Python-3.2.2.tgz
 ! [ -f ~/local/src/Python-2.7.2.tgz       ] && curl http://www.python.org/ftp/python/2.7.2/Python-2.7.2.tgz -o ~/local/src/Python-2.7.2.tgz
