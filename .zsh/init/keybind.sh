4ce344cac8bc72b69b8bce9c0fd8dac3c2c76249
Takumi IINO <trot.thunder@gmail.com>
2012-01-05 11:29:22 +0900

    zsh: wildcard history search

--- .zsh/init/keybind.sh
+++ .zsh/init/keybind.sh
@@ -3,12 +3,19 @@
 # ----------------------
 function keybind_configuration() {
     bindkey -e
+
     # historical backward/forward search with linehead string binded to ^P/^N
-    autoload history-search-end
-    zle -N history-beginning-search-backward-end history-search-end
-    zle -N history-beginning-search-forward-end history-search-end
-    bindkey "^P" history-beginning-search-backward-end
-    bindkey "^N" history-beginning-search-forward-end
+    if zle -la | grep -q '^history-incremental-pattern-search'; then
+        # zsh 4.3.10 以降でのみ有効
+        bindkey '^R' history-incremental-pattern-search-backward
+        bindkey '^S' history-incremental-pattern-search-forward
+    else
+        autoload history-search-end
+        zle -N history-beginning-search-backward-end history-search-end
+        zle -N history-beginning-search-forward-end history-search-end
+        bindkey "^P" history-beginning-search-backward-end
+        bindkey "^N" history-beginning-search-forward-end
+    fi
     [[ $EMACS = t ]] && unsetopt zle
 }
 keybind_configuration; unset -f keybind_configuration
