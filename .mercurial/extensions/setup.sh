4fb7c488ad673a18fd507dcaff79e0a9493ef124
Takumi IINO <trot.thunder@gmail.com>
2012-01-28 11:02:04 +0900

    mercurial: update hg collapse url

--- .mercurial/extensions/setup.sh
+++ .mercurial/extensions/setup.sh
@@ -5,5 +5,5 @@
 ! [ -d 'hgsubversion' ] && hg clone https://bitbucket.org/durin42/hgsubversion/
 ! [ -d 'hg-review'    ] && hg clone https://bitbucket.org/sjl/hg-review
 ! [ -d 'hg-git'       ] && hg clone https://bitbucket.org/durin42/hg-git
-! [ -d 'hgcollapse'   ] && hg clone https://bitbucket.org/ccaughie/hgcollapse
+! [ -d 'hgcollapse'   ] && hg clone https://bitbucket.org/peerst/hgcollapse
 ! [ -d 'hgflow'       ] && hg clone https://bitbucket.org/yujiewu/hgflow
