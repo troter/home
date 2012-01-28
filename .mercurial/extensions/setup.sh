241ca8e90b7a79f2832dd80c64c656d359e21de1
Takumi IINO <trot.thunder@gmail.com>
2012-01-28 11:11:43 +0900

    mercurial: sort lines

--- .mercurial/extensions/setup.sh
+++ .mercurial/extensions/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-! [ -d 'hgshelve'     ] && hg clone https://bitbucket.org/tksoh/hgshelve
-! [ -d 'hgattic'      ] && hg clone https://bitbucket.org/Bill_Barry/hgattic
-! [ -d 'hgtasks'      ] && hg clone https://bitbucket.org/alu/hgtasks
-! [ -d 'histedit'     ] && hg clone https://bitbucket.org/spookylukey/histedit
-! [ -d 'hgsubversion' ] && hg clone https://bitbucket.org/durin42/hgsubversion/
-! [ -d 'hg-review'    ] && hg clone https://bitbucket.org/sjl/hg-review
 ! [ -d 'hg-git'       ] && hg clone https://bitbucket.org/durin42/hg-git
+! [ -d 'hg-review'    ] && hg clone https://bitbucket.org/sjl/hg-review
+! [ -d 'hgattic'      ] && hg clone https://bitbucket.org/Bill_Barry/hgattic
 ! [ -d 'hgcollapse'   ] && hg clone https://bitbucket.org/peerst/hgcollapse
 ! [ -d 'hgflow'       ] && hg clone https://bitbucket.org/yujiewu/hgflow
+! [ -d 'hgshelve'     ] && hg clone https://bitbucket.org/tksoh/hgshelve
+! [ -d 'hgsubversion' ] && hg clone https://bitbucket.org/durin42/hgsubversion/
+! [ -d 'hgtasks'      ] && hg clone https://bitbucket.org/alu/hgtasks
+! [ -d 'histedit'     ] && hg clone https://bitbucket.org/spookylukey/histedit
