62b3796441351f7d88dbbd22da52140ad16c3cea
Takumi IINO <trot.thunder@gmail.com>
2012-02-04 19:43:44 +0900

    local: use verion manager like rvm and pythonbrew

--- local/setup.sh
+++ local/setup.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
 ! [ -f ~/local/bin/ack                    ] && (curl http://betterthangrep.com/ack-standalone > ~/local/bin/ack && chmod 0755 ~/local/bin/ack)
-! [ -f ~/local/src/ez_setup.py            ] && (curl http://peak.telecommunity.com/dist/ez_setup.py > ~/local/src/ez_setup.py && chmod 0755 ~/local/src/ez_setup.py)
-! [ -f ~/local/src/ruby-1.9.2-p290.tar.gz ] && curl http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p290.tar.gz -o ~/local/src/ruby-1.9.2-p290.tar.gz
-! [ -f ~/local/src/Python-3.2.2.tgz       ] && curl http://www.python.org/ftp/python/3.2.2/Python-3.2.2.tgz -o ~/local/src/Python-3.2.2.tgz
-! [ -f ~/local/src/Python-2.7.2.tgz       ] && curl http://www.python.org/ftp/python/2.7.2/Python-2.7.2.tgz -o ~/local/src/Python-2.7.2.tgz
 ! [ -f ~/local/src/stow-2.1.3.tar.gz     ] && curl http://ftp.gnu.org/gnu/stow/stow-2.1.3.tar.gz -o ~/local/src/stow-2.1.3.tar.gz
 ! [ -f ~/local/src/emacs-23.3a.tar.gz     ] && curl http://ftp.gnu.org/pub/gnu/emacs/emacs-23.3a.tar.gz -o ~/local/src/emacs-23.3a.tar.gz
 ! [ -f ~/local/src/zsh-4.3.12.tar.gz      ] && curl http://ftp.jaist.ac.jp/pub/sourceforge/z/project/zs/zsh/zsh-dev/4.3.12/zsh-4.3.12.tar.gz -o ~/local/src/zsh-4.3.12.tar.gz
