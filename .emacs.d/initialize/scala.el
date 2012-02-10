e2146d67ac88ff7f4b6261020de9f7a987142ed4
Takumi IINO <trot.thunder@gmail.com>
2012-02-11 00:30:15 +0900

    emacs: fix

--- .emacs.d/initialize/scala.el
+++ .emacs.d/initialize/scala.el
@@ -1,8 +1,7 @@
 ;;; -*- coding: utf-8; indent-tabs-mode: nil -*-
 
-(block "scala-setting"
+(when (getenv "SCALA_HOME")
   (setq scala-home (getenv "SCALA_HOME"))
-  (unless scala-home (return))
   (setq scala-plugins-directory
         (expand-file-name "misc/scala-tool-support/emacs" (cygpath scala-home)))
   (when (file-exists-p scala-plugins-directory)
