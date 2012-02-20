1d3a89795ceaf4cabcf02acfad9b2ed84271a356
Takumi IINO <trot.thunder@gmail.com>
2012-02-20 19:53:41 +0900

    emacs: installing rinari with marmalade

--- .emacs.d/site-lisp/subdirs.el
+++ .emacs.d/site-lisp/subdirs.el
@@ -3,12 +3,10 @@
 ;; certain of its subdirectories.  Here we specify them.
 
 (defvar package-resource
-  '("php-mode-1.5.0" "yasnippet-0.6.1c" "color-theme-6.6.0"
-    "elscreen-1.4.6" "elscreen-dired-0.1.0" "apel-10.8"
-    "ri-emacs"
+  '("yasnippet-0.6.1c"
+    "elscreen-dired-0.1.0" "apel-10.8"
     "auto-complete-1.3.1"
-    "rinari"
-    "haskell-mode-2.8.0"))
+    "rhtml"))
 
 (setq load-path
       (merge-path-list
