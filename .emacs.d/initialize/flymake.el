37b680aeed13a4caf004a1c2d50be5c56b8ce434
Takumi IINO <trot.thunder@gmail.com>
2012-02-20 17:23:14 +0900

    emacs: installing flymake, flymake-ruby, flymake-php with marmalade

--- .emacs.d/initialize/flymake.el
+++ .emacs.d/initialize/flymake.el
@@ -1,28 +1,12 @@
 ;;; -*- coding: utf-8; indent-tabs-mode: nil -*-
 ;; refer: http://openlab.dino.co.jp/wp-content/uploads/2008/08/dot-emacs.txt
 (when (require 'flymake nil t)
-  ;; PHP用設定
-  (defvar flymake-lint-php "php")
-  (when (executable-find flymake-lint-php)
-    (when (not (fboundp 'flymake-php-init))
-      ;; flymake-php-initが未定義のバージョンだったら、自分で定義する
-      (defun flymake-php-init ()
-        (let* ((temp-file   (flymake-init-create-temp-buffer-copy
-                             'flymake-create-temp-inplace))
-               (local-file  (file-relative-name
-                             temp-file
-                             (file-name-directory buffer-file-name))))
-          (list flymake-lint-php (list "-f" local-file "-l"))))
-      (setq flymake-allowed-file-name-masks
-            (append
-             flymake-allowed-file-name-masks
-             '(("\\.php[345]?$" flymake-php-init))))
-      (setq flymake-err-line-patterns
-            (cons
-             '("\\(\\(?:Parse error\\|Fatal error\\|Warning\\): .*\\) in \\(.*\\) on line \\([0-9]+\\)" 2 3 nil 1)
-             flymake-err-line-patterns)))
-    (defun-eval-after-load 'php-mode
-      (defun-add-hook 'php-mode-hook (flymake-mode t))))
+  (defun-eval-after-load 'php-mode
+    (require 'flymake-php)
+    (add-hook 'php-mode-hook 'flymake-php-load))
+  (defun-eval-after-load 'ruby-mode
+    (require 'flymake-ruby)
+    (add-hook 'ruby-mode-hook 'flymake-ruby-load))
 
   ;; JavaScript用設定
   (defvar flymake-lint-javascript "jsl")
@@ -48,29 +32,6 @@
              '("\\(.+\\)(\\([0-9]+\\)): \\(?:lint \\)?\\(\\(?:warning\\|SyntaxError\\):.+\\)" 1 2 nil 3)
              flymake-err-line-patterns)))
     (defun-add-hook 'javascript-mode-hook (flymake-mode t)))
-
-  (defvar flymake-lint-ruby "ruby")
-  (when (executable-find flymake-lint-ruby)
-    (when (not (fboundp 'flymake-ruby-init))
-      ;; Invoke ruby with '-c' to get syntax checking
-      (defun flymake-ruby-init ()
-        (let* ((temp-file   (flymake-init-create-temp-buffer-copy
-                             'flymake-create-temp-inplace))
-               (local-file  (file-relative-name
-                             temp-file
-                             (file-name-directory buffer-file-name))))
-          (list flymake-lint-ruby (list "-c" local-file))))
-
-      (push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
-      (push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
-
-      (push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
-
-      (defun-eval-after-load 'ruby-mode
-        (defun-add-hook 'ruby-mode-hook
-          ;; Don't want flymake mode for ruby regions in rhtml files and also on read only files
-          (if (and (not (null buffer-file-name)) (file-writable-p buffer-file-name))
-              (flymake-mode t))))))
 
   (defvar flymake-lint-elisp (concat base-directory "/bin/elisplint"))
   (when (executable-find flymake-lint-elisp)
