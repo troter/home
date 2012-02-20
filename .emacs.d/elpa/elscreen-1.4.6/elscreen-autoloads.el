4b7c6248524fbce8fd0b29afd4b35fdf5e4e622b
Takumi IINO <trot.thunder@gmail.com>
2012-02-20 16:45:34 +0900

    emacs: installing elscreen with marmalade

---
+++ .emacs.d/elpa/elscreen-1.4.6/elscreen-autoloads.el
@@ -0,0 +1,18 @@
+;;; elscreen-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("elscreen-pkg.el" "elscreen.el") (20289
+;;;;;;  63919 251000))
+
+;;;***
+
+(provide 'elscreen-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; elscreen-autoloads.el ends here
