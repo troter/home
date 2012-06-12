b0d7d2bcd69465aaac0f87758bdbf77b880afa69
Takumi IINO <trot.thunder@gmail.com>
2012-06-13 00:33:41 +0900

    zsh: fixes redirect at virtualenvwrapper initialize script

--- .zsh/init/virtualenvwrapper.sh
+++ .zsh/init/virtualenvwrapper.sh
@@ -1,3 +1,3 @@
 export WORKON_HOME=$HOME/.virtualenvs
 export VIRTUALENV_USE_DISTRIBUTE=true
-which virtualenvwrapper.sh 2>&1 > /dev/null && source `which virtualenvwrapper.sh`
+which virtualenvwrapper.sh >/dev/null 2>&1 && source `which virtualenvwrapper.sh`
