3ab909845f85000e7adc078a059146c4118b49ac
Takumi IINO <trot.thunder@gmail.com>
2012-08-04 17:30:44 +0900

    mercurial: remove shelve extension

--- .mercurial/extensions/setup.sh
+++ .mercurial/extensions/setup.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 ! [ -d 'hg-git'       ] && hg clone https://bitbucket.org/durin42/hg-git
 ! [ -d 'hg-review'    ] && hg clone https://bitbucket.org/sjl/hg-review
-! [ -d 'hgattic'      ] && hg clone https://bitbucket.org/Bill_Barry/hgattic
 ! [ -d 'hgcollapse'   ] && hg clone https://bitbucket.org/peerst/hgcollapse
 ! [ -d 'hgflow'       ] && hg clone https://bitbucket.org/yujiewu/hgflow
-! [ -d 'hgshelve'     ] && hg clone https://bitbucket.org/tksoh/hgshelve
 ! [ -d 'hgsubversion' ] && hg clone https://bitbucket.org/durin42/hgsubversion/
 ! [ -d 'hgtasks'      ] && hg clone https://bitbucket.org/alu/hgtasks
