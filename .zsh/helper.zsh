af0e94ecbbe630b7420f31961fca4e2854661de7
Takumi IINO <trot.thunder@gmail.com>
2013-04-10 23:47:19 +0900

    zsh: add debug flag

--- .zsh/helper.zsh
+++ .zsh/helper.zsh
@@ -1,12 +1,16 @@
 # -*- mode: sh -*-
 
+function if_debug() {
+  [[ -n $DEBUG ]]
+}
+
 function load_directory_files() {
   local dir=$1
   local glob=$2
   if [[ -z $glob ]] { glob="*" }
   for file ($(eval echo $dir/$glob | sort)) {
-    echo -n "loding '$file' ... "
+    if_debug && echo -n "loding '$file' ... "
     source $file
-    echo "complete."
+    if_debug && echo "complete."
   }
 }
