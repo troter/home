af9259e141a5814f7f18c0c622d941a04d656c0e
Takumi IINO <trot.thunder@gmail.com>
2013-04-13 18:26:24 +0900

    sandbox: remove old file and add keep file

--- sandbox/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# -*- mode:sh; coding:utf-8-unix -*-
-
-! [ -d 'troter.github.com'       ] && git clone git@github.com:troter/troter.github.com.git
-! [ -d 's2jdbc-fluent-options'   ] && git clone git@github.com:troter/s2jdbc-fluent-options.git
-! [ -d 's2jdbc-master-slave'     ] && git clone git@github.com:troter/s2jdbc-master-slave.git
-! [ -d 's2jdbc-custom-enum-type' ] && git clone git@github.com:troter/s2jdbc-custom-enum-type.git
-! [ -d 'tags-capture'            ] && git clone git@github.com:troter/tags-capture.git
-! [ -d 'log4j-rotate'            ] && git clone git@github.com:troter/log4j-rotate.git
