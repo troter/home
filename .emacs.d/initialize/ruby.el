87d1764df82a0fa6f9a940a18f26cf86d7657d25
Takumi IINO <trot.thunder@gmail.com>
2013-04-13 23:55:57 +0900

    emacs: update auto-mode-alist for ruby

--- .emacs.d/initialize/ruby.el
+++ .emacs.d/initialize/ruby.el
@@ -4,8 +4,17 @@
   (autoload-if-found 'inf-ruby "inf-ruby" "Run an inferior Ruby process" t)
   (autoload-if-found 'inf-ruby-setup-keybindings "inf-ruby" "" t)
 
-  (dolist (regexp '("\\.rb$" "Rakefile" "\\.rake$"))
-    (add-to-list 'auto-mode-alist (cons regexp 'ruby-mode)))
+  (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
+  (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+  (add-to-list 'auto-mode-alist '("\\.thor$" . ruby-mode))
+  (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+  (add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
+  (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+  (add-to-list 'auto-mode-alist '("Thorfile$" . ruby-mode))
+  (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+  (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
+  (add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
+
   (add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
 
   (defun-eval-after-load 'ruby-mode
