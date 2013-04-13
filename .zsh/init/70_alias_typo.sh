7a4467a9015ef2bad2997ee6e6a9d8c80d9f7a80
Takumi IINO <trot.thunder@gmail.com>
2013-04-13 14:36:36 +0900

    mercurial: alias for typo

--- .zsh/init/70_alias_typo.sh
+++ .zsh/init/70_alias_typo.sh
@@ -1,2 +1,11 @@
 # -*- coding: utf-8; mode: sh; -*-
 # Alias for typo configuration.
+
+alias gh="hg"
+alias hgst="hg st"
+alias hgshow="hg show"
+alias hgdiff="hg diff"
+alias hgdiff="hg diff"
+alias hgqls="hg qls"
+alias hgqpop="hg qpop"
+alias hgqpush="hg qpush"
