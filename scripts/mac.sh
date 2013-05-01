b08d203279e249fd190f1c623470adcdf48af3cb
Takumi IINO <trot.thunder@gmail.com>
2013-05-01 20:40:54 +0900

    mac: add setup script

---
+++ scripts/mac.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $(uname) != 'Darwin' ]; then
+  echo "Should run on Mac OS X!"
+  exit 1
+fi
+
+# Show admin host info
+sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo 1
+
+# Enable locate service
+sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.locate.plist
+
+# Finder extra options.
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool TRUE
+defaults write com.apple.finder PathBarRootAtHome -bool TRUE
+defaults write com.apple.finder AppleShowAllFiles -bool TRUE
+killall Finder
+
+# don't write ds_sote on network store
+defaults write com.appple.desktopservices DSDontWriteNetworkStores -bool TRUE
