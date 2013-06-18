b31ffe7904bf871588f774383bc1e8a5dba9c3c9
Takumi IINO <trot.thunder@gmail.com>
2013-06-18 11:56:32 +0900

    userscripts:gitray: support to the github repository next

--- scripts/userscripts/gitray-link.user.js
+++ scripts/userscripts/gitray-link.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Gitray link
 // @namespace  https://bitbucket.org/troter/home/raw/default/scripts/userscripts
-// @version    0.1
+// @version    0.2
 // @description  add gitray link
 // @match      https://github.com/*
 // @copyright  2012+, Takumi IINO
@@ -9,12 +9,21 @@
 
 var $ = unsafeWindow.jQuery;
 $(function() {
-  if ($('input[name="nwo"]')) {
-    var gitray = [
-      '<li><a class="tabnav-tab" href="http://gitray.com/',
-      $('input[name="nwo"]').val(),
-      '">Gitray.com</a></li>'
-    ].join('');
-    $('ul.tabnav-tabs').last().append(gitray);
-  }
+  $('input[name="nwo"]').each(function () {
+    var repo = $(this).val();
+
+    $('ul.repo-menu').last().each(function () {
+      var gitray = [
+        '<li class="tooltipped leftwards" original-title="Gitray.com">',
+        '<a href="http://gitray.com/',
+        repo,
+        '" class="js-selected-navigation-item js-disable-pjax" data-selected-links"">',
+        '<span class="octicon octicon-link-external"></span> <span class="full-word">Gitray.com</span>',
+        '<img alt="Octocat-spinner-32" class="mini-loader" height="16" src="https://a248.e.akamai.net/assets.github.com/images/spinners/octocat-spinner-32.gif" width="16">',
+        '</a>',
+        '</li>'
+      ].join('');
+      $(this).last().append(gitray);
+    });
+  });
 });
