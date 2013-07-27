4549b3399b03e022e61faa3bd923e240cf61120d
Takumi IINO <trot.thunder@gmail.com>
2013-07-27 14:59:03 +0900

    zsh: change how to find virtualenvwrapper.sh

--- .zsh.d/init/45_virtualenvwrapper.sh
+++ .zsh.d/init/45_virtualenvwrapper.sh
@@ -1,11 +1,21 @@
 export WORKON_HOME=$HOME/.virtualenvs
 export VIRTUALENV_USE_DISTRIBUTE=true
-if exists virtualenvwrapper.sh; then
-    if which virtualenvwrapper.sh | grep '.pyenv/shims' > /dev/null; then
-        # skip.
-        # if run virtualenvwrapper.sh via pyenv wrapper script, zsh quits.
-        # because pyenv wrapper script run command with exec.
-    else
-        source $(which virtualenvwrapper.sh)
-    fi
+VIRTUALENVWRAPPER_SH=$VIRTUALENVWRAPPER_SH
+
+if [ -n "${VIRTUALENVWRAPPER_SH}" ] && [ -e ${VIRTUALENVWRAPPER_SH} ]; then
+    # use ${VIRTUALENVWRAPPER_SH}
+elif exists virtualenvwrapper.sh; then
+    VIRTUALENVWRAPPER_SH=$(which virtualenvwrapper.sh)
+elif [ -x /usr/local/share/python/virtualenvwrapper.sh ]; then
+    VIRTUALENVWRAPPER_SH=/usr/local/share/python/virtualenvwrapper.sh
+elif [ -x /usr/share/python/virtualenvwrapper.sh ]; then
+    VIRTUALENVWRAPPER_SH=/usr/local/share/python/virtualenvwrapper.sh
+fi
+
+if grep '.pyenv/shims' <(echo $VIRTUALENVWRAPPER_SH) > /dev/null; then
+    # skip.
+    # if run virtualenvwrapper.sh via pyenv wrapper script, zsh quits.
+    # because pyenv wrapper script run command with exec.
+else
+    source $VIRTUALENVWRAPPER_SH
 fi
