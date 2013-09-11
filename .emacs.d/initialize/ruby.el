7ec50d3e8d17df60932e157b81a5f037d817dc9f
Takumi IINO <trot.thunder@gmail.com>
2013-09-11 19:28:23 +0900

    emacs: define eval-after-load hook at outside of autoload hook

--- .emacs.d/initialize/ruby.el
+++ .emacs.d/initialize/ruby.el
@@ -17,35 +17,6 @@
 
   (add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
 
-  (defun-eval-after-load 'ruby-mode
-    (require 'ruby-electric nil t)
-    (require 'ruby-block nil t)
-    (require 'rspec-mode nil t)
-
-    ;; https://gist.github.com/dgutov/1274520
-    (defadvice ruby-indent-line (after unindent-closing-paren activate)
-      (let ((column (current-column))
-            indent offset)
-        (save-excursion
-          (back-to-indentation)
-          (let ((state (syntax-ppss)))
-            (setq offset (- column (current-column)))
-            (when (and (eq (char-after) ?\))
-                       (not (zerop (car state))))
-              (goto-char (cadr state))
-              (setq indent (current-indentation)))))
-        (when indent
-          (indent-line-to indent)
-          (when (> offset 0) (forward-char offset)))))
-
-    (defun-add-hook 'ruby-mode-hook
-      (exec-if-bound (inf-ruby-setup-keybindings))
-      (exec-if-bound (ruby-electric-mode t))
-      (exec-if-bound (ruby-block-mode t))
-      (setq ruby-deep-indent-paren-style nil)
-      (setq ruby-electric-expand-delimiters-list '( ?\{))
-      ))
-
 ;  (defun-eval-after-load 'auto-complete-config
 ;    (ac-rcodetools-initialize))
 
@@ -54,6 +25,35 @@
      'hs-special-modes-alist
      '(ruby-mode "class\\|module\\|def\\|if\\|unless\\|case\\|while\\|until\\|for\\|begin\\|do" "end" "#" ruby-end-of-block nil)))
   )
+
+(defun-eval-after-load 'ruby-mode
+  (require 'ruby-electric nil t)
+  (require 'ruby-block nil t)
+  (require 'rspec-mode nil t)
+
+  ;; https://gist.github.com/dgutov/1274520
+  (defadvice ruby-indent-line (after unindent-closing-paren activate)
+    (let ((column (current-column))
+          indent offset)
+      (save-excursion
+        (back-to-indentation)
+        (let ((state (syntax-ppss)))
+          (setq offset (- column (current-column)))
+          (when (and (eq (char-after) ?\))
+                     (not (zerop (car state))))
+            (goto-char (cadr state))
+            (setq indent (current-indentation)))))
+      (when indent
+        (indent-line-to indent)
+        (when (> offset 0) (forward-char offset)))))
+
+  (defun-add-hook 'ruby-mode-hook
+    (exec-if-bound (inf-ruby-setup-keybindings))
+    (exec-if-bound (ruby-electric-mode t))
+    (exec-if-bound (ruby-block-mode t))
+    (setq ruby-deep-indent-paren-style nil)
+    (setq ruby-electric-expand-delimiters-list '( ?\{))
+    ))
 
 ;; rinari
 (when (require 'rinari nil t)
