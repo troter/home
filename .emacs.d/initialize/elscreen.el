27a2a4819ce16591e6f5368d3af65078ee576904
Takumi IINO <trot.thunder@gmail.com>
2013-10-27 13:40:51 +0900

    emacs: uninstall elscreen-dired. it is included in elscreen

--- .emacs.d/initialize/elscreen.el
+++ .emacs.d/initialize/elscreen.el
@@ -1,7 +1,6 @@
 ;;; -*- coding: utf-8; indent-tabs-mode: nil -*-
 
 (when (require 'elscreen)
-  (require 'elscreen-dired)
   (define-key elscreen-map "\C-z" 'suspend-emacs)
   (defun-eval-after-load 'helm-elscreen
     (define-key elscreen-map "\C-o" 'helm-elscreen))
