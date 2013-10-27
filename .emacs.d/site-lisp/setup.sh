4fd80caf708d2532f1c0dc84588a9167db98ef18
Takumi IINO <trot.thunder@gmail.com>
2013-10-27 13:48:06 +0900

    emacs: cleanup site-lisp

--- .emacs.d/site-lisp/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-! [ -d "elscreen-dired-0.1.0" ] && curl -0 ftp://ftp.morishima.net/pub/morishima.net/naoto/ElScreen/elscreen-dired-0.1.0.tar.gz
-! [ -d "apel-10.8"            ] && curl -O http://kanji.zinbun.kyoto-u.ac.jp/~tomo/lemi/dist/apel/apel-10.8.tar.gz
