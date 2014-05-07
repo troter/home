597fb6810d44a4af6323423b8d0fa92f20ba6d72
Takumi IINO <trot.thunder@gmail.com>
2014-05-07 11:41:50 +0900

    zsh: define history search key-bind if not exist percol

--- .oh-my-zsh-custom/key-bindings.zsh
+++ .oh-my-zsh-custom/key-bindings.zsh
@@ -1,21 +1,23 @@
 # -*- coding: utf-8; mode: sh; -*-
 # Keybind configuration.
 # ----------------------
-function keybind_configuration() {
-  bindkey -e
+if ! exists percol; then
+  function keybind_configuration() {
+    bindkey -e
 
-  # historical backward/forward search with linehead string binded to ^P/^N
-  if zle -la | grep -q '^history-incremental-pattern-search'; then
-    # zsh 4.3.10 以降でのみ有効
-    bindkey '^R' history-incremental-pattern-search-backward
-    bindkey '^S' history-incremental-pattern-search-forward
-  else
-    autoload history-search-end
-    zle -N history-beginning-search-backward-end history-search-end
-    zle -N history-beginning-search-forward-end history-search-end
-    bindkey "^P" history-beginning-search-backward-end
-    bindkey "^N" history-beginning-search-forward-end
-  fi
-  [[ $EMACS = t ]] && unsetopt zle
-}
-keybind_configuration; unset -f keybind_configuration
+    # historical backward/forward search with linehead string binded to ^P/^N
+    if zle -la | grep -q '^history-incremental-pattern-search'; then
+      # zsh 4.3.10 以降でのみ有効
+      bindkey '^R' history-incremental-pattern-search-backward
+      bindkey '^S' history-incremental-pattern-search-forward
+    else
+      autoload history-search-end
+      zle -N history-beginning-search-backward-end history-search-end
+      zle -N history-beginning-search-forward-end history-search-end
+      bindkey "^P" history-beginning-search-backward-end
+      bindkey "^N" history-beginning-search-forward-end
+    fi
+    [[ $EMACS = t ]] && unsetopt zle
+  }
+  keybind_configuration; unset -f keybind_configuration
+fi
