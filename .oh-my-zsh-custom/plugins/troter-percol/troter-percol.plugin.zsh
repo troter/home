26b470db0cef240d0e7a0b8d3677a94ab7c14c4e
Takumi IINO <trot.thunder@gmail.com>
2014-06-05 12:58:35 +0900

    percol: implement percol_select_repository

--- .oh-my-zsh-custom/plugins/troter-percol/troter-percol.plugin.zsh
+++ .oh-my-zsh-custom/plugins/troter-percol/troter-percol.plugin.zsh
@@ -12,6 +12,29 @@ if [[ $(whence percol) != "" ]]; then
   zle -N percol_select_history
   bindkey '^R' percol_select_history
 
+  function _percol_select_repository_source() {
+    local -a target_dirs
+    target_dirs=(sandbox src work)
+    for dir in $target_dirs; do
+      ls -d $HOME/$dir/*/
+      [ -d $HOME/$dir/github.com ] && ls -d $HOME/$dir/github.com/*/*/
+      [ -d $HOME/$dir/bitbucket.org ] && ls -d $HOME/$dir/bitbucket.org/*/*/
+      [ -d $HOME/$dir/code.google.com ] && ls -d $HOME/$dir/code.google.com/*/*/
+    done
+  }
+
+  function percol_select_repository() {
+    local selected_dir=$(_percol_select_repository_source | percol --query "$LBUFFER")
+    if [ -n "$selected_dir" ]; then
+      BUFFER="cd ${selected_dir}"
+      CURSOR=$#BUFFER         # move cursor
+      zle accept-line
+    fi
+    zle -R -c               # refresh
+  }
+  zle -N percol_select_repository
+  bindkey '^x^f' percol_select_repository
+
   function ppgrep() {
     if [[ $1 == "" ]]; then
       PERCOL=percol
