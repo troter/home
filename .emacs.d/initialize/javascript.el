d7639105d731ea3307a5fc21e3986970d53c417e
Takumi IINO <trot.thunder@gmail.com>
2015-04-20 10:29:50 +0900

    emacs: update js2-mode settings

--- .emacs.d/initialize/javascript.el
+++ .emacs.d/initialize/javascript.el
@@ -2,6 +2,7 @@
 
 (when (autoload 'js2-mode "js2-mode" nil t)
   (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+  (add-to-list 'auto-mode-alist '("\\.es6\\'" . js2-mode))
   (defun-eval-after-load 'js2-mode
     (require 'js2-imenu-extras)
     (js2-imenu-extras-setup)))
