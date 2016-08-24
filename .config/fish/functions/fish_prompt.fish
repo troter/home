04652c6439b8ac9571bd0181fbdd367b926ad897
Takumi IINO <trot.thunder@gmail.com>
2016-08-24 16:45:10 +0900

    fish: update prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,4 +1,33 @@
 function fish_prompt --description 'Write out the prompt'
+  echo -n [
+
+  # User
+  set_color $fish_color_user
+  echo -n (whoami)
+  set_color normal
+
+  echo -n '@'
+
+  # Host
+  set_color $fish_color_host
+  echo -n (hostname -s)
+  set_color normal
+
+  echo -n ':'
+  # PWD
+  set_color $fish_color_cwd
+  echo -n (prompt_pwd)
+  set_color normal
+
+  # VCS
+  #__terlar_git_prompt
+  #__fish_hg_prompt
+  # echo
+
+  echo -n ]
+
+  echo
+
   set -l last_status $status
 
   if not test $last_status -eq 0
