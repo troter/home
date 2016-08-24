14511ed9667a259d54249798fe48da8c3b8a2b62
Takumi IINO <trot.thunder@gmail.com>
2016-08-24 17:27:51 +0900

    fish: fish_title

---
+++ .config/fish/functions/fish_title.fish
@@ -0,0 +1,16 @@
+function fish_title
+ if [ "fish" != $_ ]
+   tmux rename-window (string sub -s 1 -l 20 "$argv")
+ else
+   tmux_directory_title
+ end
+end
+
+function tmux_directory_title
+  if [ "$PWD" != "$LPWD" ]
+    set LPWD "$PWD"
+    set INPUT $PWD
+    set SUBSTRING (eval echo $INPUT| awk '{ print substr( $0, length($0) - 19, length($0) ) }')
+    tmux rename-window "..$SUBSTRING"
+  end
+end
