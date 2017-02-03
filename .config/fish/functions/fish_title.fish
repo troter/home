b666e25f07798e8db95cf54e96974e6f881b1f63
Takumi IINO <trot.thunder@gmail.com>
2017-02-03 17:43:11 +0900

    fish: tmux guard

--- .config/fish/functions/fish_title.fish
+++ .config/fish/functions/fish_title.fish
@@ -1,9 +1,11 @@
 function fish_title
- if [ "fish" != $_ ]
-   tmux rename-window (string sub -s 1 -l 20 "$argv")
- else
-   tmux_directory_title
- end
+  if tmux info 2>&1 > /dev/null
+    if [ "fish" != $_ ]
+      tmux rename-window (string sub -s 1 -l 20 "$argv")
+    else
+      tmux_directory_title
+    end
+  end
 end
 
 function tmux_directory_title
