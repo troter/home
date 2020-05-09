0fe941cef881214c8bcb10e9cc8a1c6ade809e88
Takumi IINO <trot.thunder@gmail.com>
2020-05-10 02:01:44 +0900

    fish: add pipx support

--- .config/fish/conf.d/100-python.fish
+++ .config/fish/conf.d/100-python.fish
@@ -3,3 +3,7 @@ set -x PYTHONSTARTUP $HOME/.pythonstartup.py
 
 # pipsi
 set -x PATH $HOME/.local/bin $PATH
+
+if command -v register-python-argcomplete > /dev/null 2>&1
+  register-python-argcomplete --shell fish pipx | .
+end
