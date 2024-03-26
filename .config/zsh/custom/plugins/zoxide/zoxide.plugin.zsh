80b8a986d45bd97b1d5e839a062c78a158aaf0ae
Takumi IINO <trot.thunder@gmail.com>
2024-03-26 09:29:27 +0900

    zoxide: use reset-prompt and accept-line

--- .config/zsh/custom/plugins/zoxide/zoxide.plugin.zsh
+++ .config/zsh/custom/plugins/zoxide/zoxide.plugin.zsh
@@ -4,9 +4,15 @@ ZOXIDE_CMD_OVERRIDE=cd
 eval "$(zoxide init --cmd ${ZOXIDE_CMD_OVERRIDE:-z} zsh)"
 
 function zoxide-cd-widget() {
-  __zoxide_zi "${LBUFFER}"
-  BUFFER=""
-  \builtin zle reset-prompt
+  \builtin local result
+  result="$(\command zoxide query --interactive -- "${LBUFFER}")"
+  if [[ -n "${result}" ]]; then
+    BUFFER="cd ${(q-)result}"
+    \builtin zle reset-prompt
+    \builtin zle accept-line
+  else
+    \builtin zle reset-prompt
+  fi
 }
 zle -N zoxide-cd-widget
 bindkey -M emacs '^xb' zoxide-cd-widget
