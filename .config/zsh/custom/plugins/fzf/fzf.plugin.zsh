2babf61f75b2ee05313a1619d3aec687e1834f4e
Takumi IINO <trot.thunder@gmail.com>
2024-06-26 09:40:50 +0900

    fzf: enable tmux integration when run on tmux

--- .config/zsh/custom/plugins/fzf/fzf.plugin.zsh
+++ .config/zsh/custom/plugins/fzf/fzf.plugin.zsh
@@ -1,7 +1,11 @@
 (( ${+commands[fzf]} )) || return
 
 export FZF_DEFAULT_OPTS="--layout=reverse --inline-info"
-export FZF_TMUX=1
+if [[ "$TERM_PROGRAM" == "tmux" ]]; then
+  export FZF_TMUX=1
+else
+  export FZF_TMUX=0
+fi
 FZF_CTRL_T_COMMAND= FZF_ALT_C_COMMAND= eval "$(fzf --zsh)"
 
 zle -N fzf-file-widget
