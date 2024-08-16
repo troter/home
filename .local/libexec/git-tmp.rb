99b9ca3ebc92f649debf3233f51c1f21f914074a
Takumi IINO <trot.thunder@gmail.com>
2024-08-16 21:50:38 +0900

    git-tmp: add namespace

--- .local/libexec/git-tmp.rb
+++ .local/libexec/git-tmp.rb
@@ -42,31 +42,33 @@ module TTY
   end
 end
 
-class GitTmp < Thor
-  desc "branch", "create temporary branch and switch"
-  def branch
-    prefix = cmd.git_config.fetch('tmp-branch.prefix', 'tmp')
-    cmd.git("switch", "-c", generate_temporary_branch_name(prefix: prefix))
-  end
-
-  desc "merge COMMIT", "create temporary branch and merge COMMIT"
-  def merge(commit)
-    prefix = cmd.git_config.fetch('tmp-merge.prefix', 'tmp-merge')
-    cmd.git("switch", "-c", generate_temporary_branch_name(prefix: prefix))
-    cmd.git("git", "merge", commit)
-  end
-
-  no_commands do
-    def generate_temporary_branch_name(prefix:)
-      [prefix, SecureRandom.uuid].join("/")
+module GitTmp
+  class CLI < Thor
+    desc "branch", "create temporary branch and switch"
+    def branch
+      prefix = cmd.git_config.fetch('tmp-branch.prefix', 'tmp')
+      cmd.git("switch", "-c", generate_temporary_branch_name(prefix: prefix))
     end
 
-    def cmd
-      @cmd ||= TTY::Command.new.tap { |cmd| cmd.extend TTY::Command::GitRunnable }
+    desc "merge COMMIT", "create temporary branch and merge COMMIT"
+    def merge(commit)
+      prefix = cmd.git_config.fetch('tmp-merge.prefix', 'tmp-merge')
+      cmd.git("switch", "-c", generate_temporary_branch_name(prefix: prefix))
+      cmd.git("git", "merge", commit)
+    end
+
+    no_commands do
+      def generate_temporary_branch_name(prefix:)
+        [prefix, SecureRandom.uuid].join("/")
+      end
+
+      def cmd
+        @cmd ||= TTY::Command.new.tap { |cmd| cmd.extend TTY::Command::GitRunnable }
+      end
     end
   end
 end
 
 if __FILE__ == $0
-  GitTmp.start(ARGV)
+  GitTmp::CLI.start(ARGV)
 end
