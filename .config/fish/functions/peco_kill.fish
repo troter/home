ee0c53deb316d2a28ac353ea48d1b0c9ee67936f
Takumi IINO <trot.thunder@gmail.com>
2026-05-04 16:16:23 +0900

    fix(fish): use commandline -b instead of zsh-only $LBUFFER in peco_kill

--- .config/fish/functions/peco_kill.fish
+++ .config/fish/functions/peco_kill.fish
@@ -1,3 +1,3 @@
 function peco_kill
-  ps ax -o pid,time,command | eval $PECO --query "$LBUFFER" | awk '{print $1}' | xargs kill
+  ps ax -o pid,time,command | eval $PECO --query (commandline -b) | awk '{print $1}' | xargs kill
 end
