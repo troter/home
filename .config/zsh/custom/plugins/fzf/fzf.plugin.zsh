6e6a656d85343a2fd63a545717c4c1a1e4865a7a
Takumi IINO <trot.thunder@gmail.com>
2026-05-04 23:05:28 +0900

    feat(zsh): use fd as fzf-cd source for cleaner directory listing

--- .config/zsh/custom/plugins/fzf/fzf.plugin.zsh
+++ .config/zsh/custom/plugins/fzf/fzf.plugin.zsh
@@ -1,12 +1,15 @@
 (( ${+commands[fzf]} )) || return
 
 export FZF_DEFAULT_OPTS="--layout=reverse --inline-info"
+export FZF_ALT_C_COMMAND='fd --type d --hidden --strip-cwd-prefix --exclude .git --exclude node_modules'
+
 if [[ "$TERM_PROGRAM" == "tmux" ]]; then
   export FZF_TMUX=1
 else
   export FZF_TMUX=0
 fi
-FZF_CTRL_T_COMMAND= FZF_ALT_C_COMMAND= eval "$(fzf --zsh)"
+
+eval "$(fzf --zsh)"
 
 zle -N fzf-file-widget
 bindkey -M emacs '^xf' fzf-file-widget
