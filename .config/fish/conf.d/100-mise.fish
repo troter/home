6b80e082c4796da13d8562f73e95d12d6bc4e803
Takumi IINO <trot.thunder@gmail.com>
2026-05-04 16:28:25 +0900

    fix(fish): correct ttps:// typo in mise install hint

--- .config/fish/conf.d/100-mise.fish
+++ .config/fish/conf.d/100-mise.fish
@@ -1,6 +1,6 @@
 if command -v mise &> /dev/null
   if ! command -v usage &> /dev/null
-    echo "please install usage. ttps://usage.jdx.dev/cli/" >&2
+    echo "please install usage. https://usage.jdx.dev/cli/" >&2
   end
 
   mise activate fish | source
